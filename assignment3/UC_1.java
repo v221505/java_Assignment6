1f6adb8f63f6a3152a3ab33d6945e703614c674a
vishwajeet <vishwajeets253@gmail.com>
2022-10-25 22:43:07 +0530

    assignment3and4

---
+++ assignment3/UC_1.java
@@ -0,0 +1,13 @@
+public class UC_1 {
+    public static void main(String[] args){
+
+            if (Math.random() < 0.5)
+         {
+                System.out.println("Present");
+            }else{
+                System.out.println("Absent");
+            }
+
+    }
+
+}
---
+++ assignment4/UC_2.java
@@ -0,0 +1,7 @@
+public class UC_2 {
+    public static void main(String[] args){
+        int dice = (int)(Math.random()*6)+1;
+        System.out.println(dice);
+
+    }
+}
